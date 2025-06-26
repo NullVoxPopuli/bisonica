6e3c49fe460f0c0dfc87ea287b28b67f1bde25d5
NullVoxPopuli <199018+NullVoxPopuli@users.noreply.github.com>
2025-06-26 11:42:55 -0400

    Support passing an element

--- source/chart.js
+++ source/chart.js
@@ -33,100 +33,102 @@ import { menu } from './menu.js'
  * @return {function(object)} renderer
  */
 const render = (s, _panelDimensions) => {
-	let tooltipHandler
-	let errorHandler = console.error
-	let tableRenderer = table
+  let tooltipHandler
+  let errorHandler = console.error
+  let tableRenderer = table
 
-	const renderer = selection => {
-		try {
-			selection.html('')
+  const renderer = selection => {
+    selection = selection instanceof Element ? select(selection) : selection;
 
-			let panelDimensions = dimensions(s, selection.node(), _panelDimensions)
+    try {
+      selection.html('')
 
-			selection.call(setupNode(s, panelDimensions))
+      let panelDimensions = dimensions(s, selection.node(), _panelDimensions)
 
-			initializeInteractions(selection.node(), s)
+      selection.call(setupNode(s, panelDimensions))
 
-			const chartNode = selection.select('div.chart')
+      initializeInteractions(selection.node(), s)
 
-			chartNode.call(audio(s))
+      const chartNode = selection.select('div.chart')
 
-			initializeInteractions(chartNode.node(), s)
+      chartNode.call(audio(s))
 
-			chartNode.call(menu(s))
+      initializeInteractions(chartNode.node(), s)
 
-			chartNode.call(tableToggle(s, tableRenderer))
+      chartNode.call(menu(s))
 
-			// render legend
-			if (feature(s).hasLegend()) {
-				chartNode.select('.legend').call(legend(s))
-			}
-			const legendHeight = chartNode.select('.legend').node()?.getBoundingClientRect().height || 0
+      chartNode.call(tableToggle(s, tableRenderer))
 
-			const svg = chartNode.select('svg')
-			const imageHeight = panelDimensions.y - legendHeight
+      // render legend
+      if (feature(s).hasLegend()) {
+        chartNode.select('.legend').call(legend(s))
+      }
+      const legendHeight = chartNode.select('.legend').node()?.getBoundingClientRect().height || 0
 
-			svg.attr('height', Math.max(imageHeight, 0))
+      const svg = chartNode.select('svg')
+      const imageHeight = panelDimensions.y - legendHeight
 
-			svg.call(defs(s))
+      svg.attr('height', Math.max(imageHeight, 0))
 
-			const { top, right, bottom, left } = margin(s, panelDimensions)
+      svg.call(defs(s))
 
-			// subtract rendered height of legend from dimensions
-			const graphicDimensions = {
-				x: panelDimensions.x - left - right,
-				y: imageHeight - top - bottom
-			}
+      const { top, right, bottom, left } = margin(s, panelDimensions)
 
-			if (graphicDimensions.y > 0) {
-				const wrapper = chartNode
-					.select('.graphic')
-					.select('svg')
-					.call(position(s, { x: panelDimensions.x, y: imageHeight }))
-					.select(`g.${WRAPPER_CLASS}`)
+      // subtract rendered height of legend from dimensions
+      const graphicDimensions = {
+        x: panelDimensions.x - left - right,
+        y: imageHeight - top - bottom
+      }
 
-				wrapper
-					.call(axes(s, graphicDimensions))
-					.call((s.layer ? layerMarks : marks)(s, graphicDimensions))
-					.call(keyboard(s))
-					.call(interactions(s))
-				selection.call(testAttributes)
-			}
-		} catch (error) {
-			errorHandler(error)
-		}
-	}
+      if (graphicDimensions.y > 0) {
+        const wrapper = chartNode
+          .select('.graphic')
+          .select('svg')
+          .call(position(s, { x: panelDimensions.x, y: imageHeight }))
+          .select(`g.${WRAPPER_CLASS}`)
 
-	renderer.table = t => {
-		if (t === undefined) {
-			return tableRenderer
-		} else {
-			if (typeof t === 'function') {
-				tableRenderer = t
-			}
+        wrapper
+          .call(axes(s, graphicDimensions))
+          .call((s.layer ? layerMarks : marks)(s, graphicDimensions))
+          .call(keyboard(s))
+          .call(interactions(s))
+        selection.call(testAttributes)
+      }
+    } catch (error) {
+      errorHandler(error)
+    }
+  }
 
-			return renderer
-		}
-	}
+  renderer.table = t => {
+    if (t === undefined) {
+      return tableRenderer
+    } else {
+      if (typeof t === 'function') {
+        tableRenderer = t
+      }
 
-	renderer.tooltip = h => {
-		if (h === undefined) {
-			return tooltipHandler
-		} else {
-			if (typeof h === 'function') {
-				tooltipHandler = h
-				usermeta(s)
-				s.usermeta.tooltipHandler = true
-			} else {
-				throw new Error(`tooltip handler must be a function, but input was of type ${typeof h}`)
-			}
+      return renderer
+    }
+  }
 
-			return renderer
-		}
-	}
-	renderer.error = h => typeof h !== 'undefined' ? (errorHandler = h, renderer) : errorHandler
+  renderer.tooltip = h => {
+    if (h === undefined) {
+      return tooltipHandler
+    } else {
+      if (typeof h === 'function') {
+        tooltipHandler = h
+        usermeta(s)
+        s.usermeta.tooltipHandler = true
+      } else {
+        throw new Error(`tooltip handler must be a function, but input was of type ${typeof h}`)
+      }
 
-	return renderer
+      return renderer
+    }
+  }
+  renderer.error = h => typeof h !== 'undefined' ? (errorHandler = h, renderer) : errorHandler
+
+  return renderer
 }
 
 /**
@@ -137,15 +139,15 @@ const render = (s, _panelDimensions) => {
  * @return {function(object)} asynchronous rendering function
  */
 const asyncRender = (s, dimensions) => {
-	const renderer = render(s, dimensions)
-	const fn = selection => {
-		fetchAll(s)
-			.then(() => {
-				selection.call(renderer)
-			})
-	}
-	copyMethods(['error', 'tooltip', 'table'], renderer, fn)
-	return fn
+  const renderer = render(s, dimensions)
+  const fn = selection => {
+    fetchAll(s)
+      .then(() => {
+        selection.call(renderer)
+      })
+  }
+  copyMethods(['error', 'tooltip', 'table'], renderer, fn)
+  return fn
 }
 
 /**
@@ -156,11 +158,11 @@ const asyncRender = (s, dimensions) => {
  * @return {function(object)} renderer
  */
 const chart = (s, dimensions) => {
-	if (s.data?.url || s.layer?.find(layer => layer.data?.url)) {
-		return asyncRender(s, dimensions)
-	} else {
-		return render(s, dimensions)
-	}
+  if (s.data?.url || s.layer?.find(layer => layer.data?.url)) {
+    return asyncRender(s, dimensions)
+  } else {
+    return render(s, dimensions)
+  }
 }
 
 export { chart }
